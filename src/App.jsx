cafeaf716bf1d8d8dac5471e5f4c4a4bebddb07f
MMYUGESH <mmyugesh@gmail.com>
2023-07-05 02:32:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      name: "Hari",
+      description: "Reactjs Developer",
+
+    },
+    {
+      name: "Yugesh",
+      description: "MERN stack developer",
+
+    },
+    {
+      name: "Deepak",
+      description: "Full stack developer",
+
+    }, {
+      name: "Venkat",
+      description: "Nodejs Developer",
+
+    },
+    {
+      name: "Gokul",
+      description: "MEAN stack developer",
+
+    }
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+        rows.map((currRow, idx) => {
+          if (idx !== rowToEdit) return currRow;
+
+          return newRow;
+        })
+      );
+  };
+
+  return (
+    <div className="App">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
